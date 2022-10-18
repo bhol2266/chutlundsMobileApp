83a1ba35055447492e55ca21b1640df15e05e22d
bhol2266 <bhola2266@gmail.com>
2022-10-19 01:20:20 +0545

    first commit

--- app/src/main/java/com/bhola/chutlundsmobileapp/MainActivity.java
+++ app/src/main/java/com/bhola/chutlundsmobileapp/MainActivity.java
@@ -6,6 +6,9 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import com.android.volley.AuthFailureError;
 import com.android.volley.NetworkResponse;
@@ -17,78 +20,76 @@ import com.android.volley.VolleyLog;
 import com.android.volley.toolbox.HttpHeaderParser;
 import com.android.volley.toolbox.StringRequest;
 import com.android.volley.toolbox.Volley;
+import com.squareup.picasso.Picasso;
 
+import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
 import java.io.UnsupportedEncodingException;
+import java.sql.Time;
+import java.util.HashMap;
+import java.util.Map;
 
 public class MainActivity extends AppCompatActivity {
+    TextView dataText;
+    ImageView image;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Button fetchBtn=findViewById(R.id.fetchBtn);
+        Button fetchBtn = findViewById(R.id.fetchBtn);
+        dataText = findViewById(R.id.dataText);
+        image = findViewById(R.id.image);
 
         fetchBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-        getAPI_DATA();
+                getAPI_DATA();
 
             }
         });
     }
 
     private void getAPI_DATA() {
-        try
-        {
-            String API_URL="https://www.chutlunds.live/api/spangbang/getvideos";
-            RequestQueue requestQueue = Volley.newRequestQueue(this);
+        try {
+            String API_URL = "https://www.chutlunds.live/api/spangbang/getvideos";
             String url = "https://spankbang.com/s/boobs";
             JSONObject jsonBody = new JSONObject();
             jsonBody.put("url", url);
             final String requestBody = jsonBody.toString();
 
-            StringRequest stringRequest = new StringRequest(Request.Method.POST, API_URL, new Response.Listener<String>() {
-                @Override
-                public void onResponse(String response) {
-                    Log.d("VOLLEY", response);
-                    Log.d("VOLLEY2", response);
-                }
-            }, new Response.ErrorListener() {
-                @Override
-                public void onErrorResponse(VolleyError error) {
-                    Log.d("VOLLEY", error.getMessage());
-                }
-            }) {
-                @Override
-                public String getBodyContentType() {
-                    return "application/json; charset=utf-8";
-                }
+            StringRequest stringRequest = new StringRequest(Request.Method.POST, API_URL,
+                    response -> {
+                        Toast.makeText(this, "DATA Fetched", Toast.LENGTH_SHORT).show();
+                        try {
+                            JSONObject jsonObject = new JSONObject(response);
+                            JSONArray finalDataArray = jsonObject.getJSONArray(("finalDataArray"));
+                            dataText.setText((CharSequence) finalDataArray.getJSONObject(4).toString());
+                            String imageURL =finalDataArray.getJSONObject(4).getString("thumbnailArray");
+                            Picasso.with(MainActivity.this).load(imageURL).into(image);
 
-                @Override
-                public byte[] getBody() throws AuthFailureError {
-                    try {
-                        return requestBody == null ? null : requestBody.getBytes("utf-8");
-                    } catch (UnsupportedEncodingException uee) {
-                        VolleyLog.wtf("Unsupported Encoding while trying to get the bytes of %s using %s", requestBody, "utf-8");
-                        return null;
-                    }
-                }
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
 
+                    },
+                    error -> Toast.makeText(this, "", Toast.LENGTH_SHORT).show()) {
+                //Add parameters
                 @Override
-                protected Response<String> parseNetworkResponse(NetworkResponse response) {
-                    String responseString = "";
-                    if (response != null) {
-                        responseString = String.valueOf(response.statusCode);
-                        // can get more details such as response.headers
-                    }
-                    return Response.success(responseString, HttpHeaderParser.parseCacheHeaders(response));
+                protected Map<String, String> getParams() throws AuthFailureError {
+                    //body data
+                    Map<String, String> params = new HashMap<>();
+                    params.put("url", "https://spankbang.com/s/desi/?o=trending");
+                    return params;
                 }
             };
 
+
+            RequestQueue requestQueue = Volley.newRequestQueue(this);
             requestQueue.add(stringRequest);
+
         } catch (JSONException e) {
             e.printStackTrace();
         }
